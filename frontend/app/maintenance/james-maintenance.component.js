7c95d1bec306ac344c688e81945fa887840491d5
Sang NGO DOAN <sngodoan@linagora.com>
2018-02-13 14:30:58 +0700

    linagora.esn.admin#363 inject James maintenance card

---
+++ frontend/app/maintenance/james-maintenance.component.js
@@ -0,0 +1,9 @@
+(function(angular) {
+  'use strict';
+
+  angular.module('linagora.esn.james')
+    .component('jamesMaintenance', {
+      templateUrl: '/james/app/maintenance/james-maintenance.html',
+      controller: 'jamesMaintenanceController'
+    });
+})(angular);
