cf487cca81cc3b47946713119328302d1cbb1e87
Tuan Tuan LE <tlcong@linagora.com>
2019-08-12 14:41:26 +0700

    #19 fix swagger documentation

--- doc/REST_API/swagger/parameters/alias.js
+++ doc/REST_API/swagger/parameters/alias.js
@@ -2,10 +2,9 @@
  * @swagger
  * parameter:
  *   james_domain_alias:
- *     name: domain_alias
- *     in: body
+ *     name: james_domain_alias
+ *     in: path
  *     description: an alias to add/remove from a particular domain
  *     required: true
- *     schema:
- *       $ref: "#/definitions/james_domain_alias"
+ *     type: string
 **/
--- doc/REST_API/swagger/responses/token.js
+++ doc/REST_API/swagger/responses/token.js
@@ -3,5 +3,6 @@
   * response:
   *   james_token:
   *     description: The token to access to James under admin role
-  *     type: string
+  *     schema:
+  *       type: string
   */
