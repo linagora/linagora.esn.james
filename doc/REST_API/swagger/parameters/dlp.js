1d3fd3c685b8e4bee03906b41316769d9050b5eb
Thang NGUYEN MANH <tnmanh@linagora.com>
2019-12-13 10:32:10 +0700

    #27: Create backend for DLP feature

---
+++ doc/REST_API/swagger/parameters/dlp.js
@@ -0,0 +1,11 @@
+/**
+ * @swagger
+ * parameter:
+ *   james_dlp_rules:
+ *     name: james_dlp_rules
+ *     in: body
+ *     description: Dlp rules list
+ *     required: true
+ *     schema:
+ *       $ref: "#/definitions/james_dlp_rules"
+**/
---
+++ doc/REST_API/swagger/responses/dlp.js
@@ -0,0 +1,12 @@
+/**
+ * @swagger
+ * response:
+ *   james_dlp_rules:
+ *     description: James dlp rules list
+ *     schema:
+ *       $ref: "#/definitions/james_dlp_rules"
+ *   james_dlp_rule:
+ *     description: A James DLP rule
+ *     schema:
+ *       $ref: "#/definitions/james_dlp_rule"
+**/
