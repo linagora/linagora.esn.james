e170efcd431ab6bf8950bf92ce0bae658222ddce
Thang NGUYEN MANH <tnmanh@linagora.com>
2019-12-23 14:43:14 +0700

    #23: Delete generating token API

--- backend/webserver/api/index.js
+++ backend/webserver/api/index.js
@@ -6,7 +6,6 @@ module.exports = function(dependencies, lib) {
 
   const router = express.Router();
 
-  require('./token')(dependencies, lib, router);
   require('./sync')(dependencies, lib, router);
   require('./domain')(dependencies, lib, router);
   require('./quota')(dependencies, lib, router);
--- doc/REST_API/swagger/responses/token.js
+++
@@ -1,8 +0,0 @@
-/**
-  * @swagger
-  * response:
-  *   james_token:
-  *     description: The token to access to James under admin role
-  *     schema:
-  *       type: string
-  */
