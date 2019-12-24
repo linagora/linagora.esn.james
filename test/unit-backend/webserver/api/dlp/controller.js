0cde2cb99bec2ffe823cba73fee3924a71912c1e
Thang NGUYEN MANH <tnmanh@linagora.com>
2019-12-24 17:30:26 +0700

    #27: Add unit-backend for list and store dlp rule

--- test/unit-backend/webserver/api/dlp/controller.js
+++ test/unit-backend/webserver/api/dlp/controller.js
@@ -1,4 +1,5 @@
 const { expect } = require('chai');
+const sinon = require('sinon');
 
 describe('The webserver/api/dlp/controller module', function() {
   let controller;
@@ -37,6 +38,139 @@ describe('The webserver/api/dlp/controller module', function() {
       };
 
       controller.getRule(req, res);
+    });
+  });
+
+  describe('The listRules function', function() {
+    it('should respond 200 if successfully get list of rules', function(done) {
+      const rules = [{ id: 1 }, { id: 2 }];
+
+      clientMock.listDlpRules = sinon.stub().returns(Promise.resolve(rules));
+
+      const req = {
+        domain: { name: 'a' }
+      };
+
+      const res = {
+        status(code) {
+          try {
+            expect(clientMock.listDlpRules).to.have.been.calledOnce;
+            expect(code).to.equal(200);
+
+            return {
+              json: response => {
+                expect(response).to.deep.equal(rules);
+
+                done();
+              }
+            };
+          } catch (error) {
+            done(error);
+          }
+        }
+      };
+
+      controller.listRules(req, res);
+    });
+
+    it('should respond 500 if failed get list of rules', function(done) {
+      clientMock.listDlpRules = sinon.stub().returns(Promise.reject(new Error('Something wrong')));
+
+      const req = {
+        domain: { name: 'a' }
+      };
+
+      const res = {
+        status(code) {
+          try {
+            expect(clientMock.listDlpRules).to.have.been.calledOnce;
+            expect(code).to.equal(500);
+
+            return {
+              json(json) {
+                expect(json).to.deep.equal({
+                  error: {
+                    code: 500,
+                    message: 'Server Error',
+                    details: `Error while getting DLP rules for domain "${req.domain.name}"`
+                  }
+                });
+                done();
+              }
+            };
+          } catch (error) {
+            done(error);
+          }
+        }
+      };
+
+      controller.listRules(req, res);
+    });
+  });
+
+  describe('The storeRules function', function() {
+    it('should respond 204 if successfully store list of rules', function(done) {
+      const rules = [{ id: 1 }, { id: 2 }];
+
+      clientMock.storeDlpRules = sinon.stub().returns(Promise.resolve());
+
+      const req = {
+        domain: { name: 'a' },
+        body: rules
+      };
+
+      const res = {
+        status(code) {
+          try {
+            expect(clientMock.storeDlpRules).to.have.been.calledOnce;
+            expect(clientMock.storeDlpRules).to.have.been.calledWith(req.domain.name, { rules: req.body });
+            expect(code).to.equal(204);
+            done();
+
+            return { end: () => {} };
+          } catch (error) {
+            done(error);
+
+            return { end: () => {} };
+          }
+        }
+      };
+
+      controller.storeRules(req, res);
+    });
+
+    it('should respond 500 if failed store list of rules', function(done) {
+      clientMock.storeDlpRules = sinon.stub().returns(Promise.reject(new Error('Something wrong')));
+
+      const req = {
+        domain: { name: 'a' }
+      };
+
+      const res = {
+        status(code) {
+          try {
+            expect(clientMock.storeDlpRules).to.have.been.calledOnce;
+            expect(code).to.equal(500);
+
+            return {
+              json(json) {
+                expect(json).to.deep.equal({
+                  error: {
+                    code: 500,
+                    message: 'Server Error',
+                    details: `Error while storing DLP rules for domain "${req.domain.name}"`
+                  }
+                });
+                done();
+              }
+            };
+          } catch (error) {
+            done(error);
+          }
+        }
+      };
+
+      controller.storeRules(req, res);
     });
   });
 });
